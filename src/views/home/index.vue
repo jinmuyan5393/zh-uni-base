815734c95a6b759e917d0b9f0202416d212be582
jinmuyan5393 <jinmuyan5393@163.com>
2024-06-09 10:00:47 +0800

    测试工具函数

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,10 +4,24 @@
  * @LastEditTime: 2024-05-16 16:10:29
 -->
 <template>
-  <div class="home bg-white h-full p-[10px] rounded-lg overflow-auto">首页</div>
+  <div class="home bg-white h-full p-[10px] rounded-lg overflow-auto">
+    首页 --{{ num }}--
+    <el-button @click="changeLocal">变更</el-button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { storageLocal } from "@zhonghe/utils";
+storageLocal.setItem("num", 123);
+const num = computed(() => {
+  return storageLocal.getItem("num");
+});
+
+function changeLocal() {
+  storageLocal.setItem("num", Math.random() * 100);
+}
+</script>
 
 <style lang="scss" scoped>
 .home {
